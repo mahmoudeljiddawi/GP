ad73f94df4430ea75ff992898ac750aa329d4762
Mahmoud Eljiddawi <Mahmoudeljiddawi@gmail.com>
2018-12-17 05:45:58 +0200

    Initial commit

---
+++ main.py
@@ -0,0 +1,10 @@
+import cv2
+import numpy as np
+
+path ='/Users/mac/Desktop/test.jpg'
+
+BGR = cv2.imread(path)
+LAB = cv2.cvtColor(BGR , cv2.COLOR_BGR2LAB)
+origin = cv2.cvtColor(LAB , cv2.COLOR_LAB2BGR)
+cv2.imshow('i' , origin)
+cv2.waitKey()
